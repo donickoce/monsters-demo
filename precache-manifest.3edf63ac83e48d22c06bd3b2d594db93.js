e1cd2695e476e5f9b59ebf6d8f9d97e53d21c28d
Lazar <layarkostic94@gmail.com>
2019-07-29 11:14:55 +0200

    Add files via upload

---
+++ precache-manifest.3edf63ac83e48d22c06bd3b2d594db93.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "38d8a0788d19e905303607dc10a39989",
+    "url": "/monsters-demo/index.html"
+  },
+  {
+    "revision": "65227ee4bf7c6156f849",
+    "url": "/monsters-demo/static/css/main.c0c65d4b.chunk.css"
+  },
+  {
+    "revision": "eb5d03ae7c9a30028659",
+    "url": "/monsters-demo/static/js/2.4e81ba5b.chunk.js"
+  },
+  {
+    "revision": "65227ee4bf7c6156f849",
+    "url": "/monsters-demo/static/js/main.69ee55a7.chunk.js"
+  },
+  {
+    "revision": "1afd488ec0da53f12a99",
+    "url": "/monsters-demo/static/js/runtime~main.be016eab.js"
+  }
+]);
